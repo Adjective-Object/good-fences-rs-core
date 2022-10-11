44b446c143ae826119be42d7e1f5f291ac96cfc4
Edgar <edgar21_9@hotmail.com>
2022-10-11 13:25:10 -0500

    returned to original

--- scripts/is-any-new-version.sh
+++ scripts/is-any-new-version.sh
@@ -15,38 +15,8 @@ set -eo
 SCRIPTDIR=`dirname $0`
 ROOT=`realpath $SCRIPTDIR/..`
 
-function isNewVersion() {
-    _PKGJSON_PATH=$1
-
-    _REPO_ROOT=`git rev-parse --show-toplevel`
-
-    _CURRENT_PKGJSON=`jq .version "$_REPO_ROOT/$_PKGJSON_PATH"`
-    if [[ "$_CURRENT_PKGJSON" == "" ]]; then
-        echo "failed to parse git ref HEAD" >&2
-        exit 1
-    fi
-    _PRIOR_PKGJSON=$(git show HEAD^1:"$_PKGJSON_PATH" | jq .version)
-    if [[ "$_PRIOR_PKGJSON" == "" ]]; then
-        echo "failed to parse git ref HEAD" >&2
-        exit 1
-    fi
-
-    echo -n "checking $_PKGJSON_PATH for version change .. "
-
-    if [[ "$_CURRENT_PKGJSON" != "$_PRIOR_PKGJSON" ]]; then
-        echo "bump $_PRIOR_PKGJSON -> $_CURRENT_PKGJSON"
-        return 0
-    else
-        echo "no bump $_PRIOR_PKGJSON"
-        return 1
-    fi
-}
-NPM_PUBLISHED_PACKAGES=(
-    "package.json" 
-    "npm/darwin-x64/package.json"
-    "npm/win32-x64-msvc/package.json"
-    "npm/linux-x64-gnu/package.json"
-)
+source "$SCRIPTDIR/_isnewversion.sh"
+source "$SCRIPTDIR/_npm_published_packages.sh"
 
 ANY_NEW_VERSION=0
 
