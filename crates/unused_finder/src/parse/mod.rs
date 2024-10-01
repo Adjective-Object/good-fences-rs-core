9c4a2c0fc054845dc2c1c7b55b6e4866692f4cf5
Max Huang-Hobbs <Maxwell.HuangHobbs@microsoft.com>
2024-10-01 21:11:47 +0000

    cargo fmt --fix

--- crates/unused_finder/src/parse/mod.rs
+++ crates/unused_finder/src/parse/mod.rs
@@ -1,8 +1,8 @@
 pub mod data;
 pub mod exports_visitor;
+pub mod exports_visitor_runner;
 #[cfg(test)]
 pub mod exports_visitor_tests;
-pub mod exports_visitor_runner;
 
 pub use data::*;
 pub use exports_visitor_runner::get_file_import_export_info;
