d9b87c231ee33ab9e3743d2f10b0dda739929cd2
Max Huang-Hobbs <Maxwell.HuangHobbs@microsoft.com>
2024-10-21 21:50:53 +0000

    unused_finder: strip out jwalk and use ignore instead

--- crates/unused_finder/src/cfg.rs
+++ crates/unused_finder/src/cfg.rs
@@ -1,8 +1,6 @@
 use ahashmap::AHashSet;
-use anyhow::Result;
 use js_err::JsErr;
 use serde::Deserialize;
-use std::sync::Arc;
 
 /// A JSON serializable proxy for the UnusedFinderConfig struct
 ///
@@ -28,30 +26,28 @@ pub struct UnusedFinderJSONConfig {
 /// Configuration for the unused symbols finder
 #[derive(Debug, Default, Clone)]
 pub struct UnusedFinderConfig {
-    // If true, the finder should report exported symbols that are not used anywhere in the project
+    /// If true, the finder should report exported symbols that are not used anywhere in the project
     pub report_exported_symbols: bool,
 
-    // Path to the root directory of the repository
+    /// Path to the root directory of the repository
     pub repo_root: String,
 
-    // Pats to walk as "internal" source files
+    /// Pats to walk as "internal" source files
     pub root_paths: Vec<String>,
 
-    // Path to the root directory of the repository.
+    /// packages we should consider as "entry" packages
     pub entry_packages: AHashSet<String>,
-    pub skipped_dirs: Arc<Vec<glob::Pattern>>,
+
+    /// Globs of individual files & directories to skip during the file walk.
+    ///
+    /// Some internal directories are always skipped.
+    /// See [crate::walk::DEFAULT_SKIPPED_DIRS] for more details.
+    pub skip: Vec<String>,
 }
 
 impl TryFrom<UnusedFinderJSONConfig> for UnusedFinderConfig {
     type Error = JsErr;
     fn try_from(value: UnusedFinderJSONConfig) -> std::result::Result<Self, Self::Error> {
-        let skipped_dirs = value
-            .skipped_dirs
-            .iter()
-            .map(|s| glob::Pattern::new(s))
-            .collect::<Result<Vec<glob::Pattern>, _>>()
-            .map_err(JsErr::invalid_arg)?;
-
         Ok(UnusedFinderConfig {
             // raw fields that are copied from the JSON config
             report_exported_symbols: value.report_exported_symbols,
@@ -59,30 +55,7 @@ impl TryFrom<UnusedFinderJSONConfig> for UnusedFinderConfig {
             repo_root: value.repo_root,
             // other fields that are processed before use
             entry_packages: value.entry_packages.iter().cloned().collect(),
-            skipped_dirs: Arc::new(skipped_dirs),
+            skip: value.skipped_dirs,
         })
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use js_err::JsErr;
-
-    use super::{UnusedFinderConfig, UnusedFinderJSONConfig};
-
-    #[test]
-    fn test_error_in_glob() {
-        let result: Result<UnusedFinderConfig, JsErr> = (UnusedFinderJSONConfig {
-            root_paths: vec!["tests/unused_finder".to_string()],
-            repo_root: "tests/unused_finder".to_string(),
-            skipped_dirs: vec![".....///invalidpath****".to_string()],
-            ..Default::default()
-        })
-        .try_into();
-        assert!(result.is_err());
-        assert_eq!(
-            result.unwrap_err().message(),
-            "Pattern syntax error near position 21: wildcards are either regular `*` or recursive `**`"
-        )
     }
 }
