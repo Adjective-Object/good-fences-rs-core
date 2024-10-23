9b081b442ccae27a7b8e95514e0bf51495593c32
Max Huang-Hobbs <Maxwell.HuangHobbs@microsoft.com>
2024-10-23 22:30:12 +0000

    unused_finder: fix bug where package path patterns failed to match because of leading "./" prefix

--- crates/unused_finder/src/cfg/mod.rs
+++ crates/unused_finder/src/cfg/mod.rs
@@ -1,10 +1,9 @@
-use std::{
-    fmt::{Display, Formatter},
-    path::Path,
-};
+use std::fmt::{Display, Formatter};
 
-use ahashmap::AHashSet;
+use package_match_rules::PackageMatchRules;
 use serde::Deserialize;
+
+mod package_match_rules;
 
 #[derive(Debug, Eq, PartialEq)]
 pub struct ErrList<E>(Vec<E>);
@@ -113,81 +112,6 @@ pub struct UnusedFinderConfig {
     /// Some internal directories are always skipped.
     /// See [crate::walk::DEFAULT_SKIPPED_DIRS] for more details.
     pub skip: Vec<String>,
-}
-
-#[derive(Debug, Default, Clone)]
-pub struct PackageMatchRules {
-    pub names: AHashSet<String>,
-    pub name_patterns: Vec<glob::Pattern>,
-    pub path_patterns: Vec<glob::Pattern>,
-}
-
-impl PackageMatchRules {
-    /// Check if a package matches the rules
-    ///
-    /// * `package_path` - The path to the package, relative to the repo root
-    /// * `package_name` - The name of the package
-    pub fn matches(&self, package_path: &Path, pacakge_name: &str) -> bool {
-        if self.names.contains(pacakge_name) {
-            return true;
-        }
-        for pattern in &self.name_patterns {
-            if pattern.matches(pacakge_name) {
-                return true;
-            }
-        }
-        for pattern in &self.path_patterns {
-            // empty string (with no heap allocated data)
-            //
-            // This is used to avoid allocating a new string for the path
-            // in the case that the path is already a string-safe OsString
-            let mut path_string: String = String::new();
-            let path_string_ref = package_path.to_str().unwrap_or_else(|| {
-                path_string = package_path.to_string_lossy().into_owned();
-                &path_string
-            });
-            if pattern.matches(path_string_ref) {
-                return true;
-            }
-        }
-
-        false
-    }
-}
-
-impl<T: AsRef<str> + ToString> TryFrom<Vec<T>> for PackageMatchRules {
-    type Error = ConfigError;
-    fn try_from(value: Vec<T>) -> Result<Self, Self::Error> {
-        let mut names = AHashSet::with_capacity_and_hasher(value.len(), Default::default());
-        let mut name_patterns = Vec::new();
-        let mut path_patterns = Vec::new();
-        let mut errs: Vec<PatErr> = Vec::new();
-        for (i, item) in value.into_iter().enumerate() {
-            if item.as_ref().starts_with("./") {
-                match glob::Pattern::new(item.as_ref()) {
-                    Err(e) => errs.push(PatErr(i, GlobInterp::Path, e)),
-                    Ok(r) => path_patterns.push(r),
-                };
-            } else if item.as_ref().chars().any(|c| "~)('!*".contains(c)) {
-                match glob::Pattern::new(item.as_ref()) {
-                    Err(e) => errs.push(PatErr(i, GlobInterp::Name, e)),
-                    Ok(r) => name_patterns.push(r),
-                };
-            } else {
-                names.insert(item.to_string());
-            }
-        }
-
-        if !errs.is_empty() {
-            return Err(ConfigError::InvalidGlobPatterns(ErrList(errs)));
-        }
-
-        Ok(Self {
-            names,
-            name_patterns,
-            path_patterns,
-        })
-    }
 }
 
 impl TryFrom<UnusedFinderJSONConfig> for UnusedFinderConfig {
