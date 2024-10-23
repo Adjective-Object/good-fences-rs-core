5105dc7cd68e45676bcc9a25dc90140a1d75d8c6
Max Huang-Hobbs <Maxwell.HuangHobbs@microsoft.com>
2024-10-23 18:00:18 +0000

    ftree_cache: enforce absolute base paths

--- crates/import_resolver/src/swc_resolver/util/to_absolute_path.rs
+++ crates/import_resolver/src/swc_resolver/util/to_absolute_path.rs
@@ -3,11 +3,15 @@ use std::path::{Path, PathBuf};
 use anyhow::Error;
 use path_clean::PathClean;
 
-pub fn to_absolute_path(path: &Path) -> Result<PathBuf, Error> {
+pub fn to_absolute_path(base: &Path, path: &Path) -> Result<PathBuf, Error> {
+    if !base.is_absolute() {
+        return Err(Error::msg("Base path must be absolute"));
+    }
+
     let absolute_path = if path.is_absolute() {
         path.to_path_buf()
     } else {
-        std::env::current_dir()?.join(path)
+        base.join(path).to_path_buf()
     }
     .clean();
 
