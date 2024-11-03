a2bf79eb483a6ee4070ebfbacb46873e52f797e8
Maxwell Huang-Hobbs <mhuan13@gmail.com>
2024-11-03 17:56:56 +0000

    fix tests

--- crates/unused_finder/src/parse/exports_visitor_tests.rs
+++ crates/unused_finder/src/parse/exports_visitor_tests.rs
@@ -1,9 +1,8 @@
 #[cfg(test)]
 mod test {
-    use std::sync::Arc;
-
     use ahashmap::{AHashMap, AHashSet};
     use swc_common::comments::{Comments, SingleThreadedComments};
+    use swc_common::sync::Lrc;
     use swc_common::{FileName, SourceFile, SourceMap};
     use swc_ecma_parser::lexer::Lexer;
     use swc_ecma_parser::{Capturing, Parser};
@@ -16,7 +15,7 @@ mod test {
     use test_tmpdir::{amap, aset};
 
     fn create_test_parser<'a>(
-        fm: &'a Arc<SourceFile>,
+        fm: &'a Lrc<SourceFile>,
         comments: Option<&'a dyn Comments>,
     ) -> Parser<Capturing<Lexer<'a>>> {
         let lexer = create_lexer(fm, comments);
@@ -27,10 +26,10 @@ mod test {
 
     #[test]
     fn test_allowed_unused_export_named() {
-        let cm = Arc::<SourceMap>::default();
+        let cm = Lrc::<SourceMap>::default();
         let comments = SingleThreadedComments::default();
         let fm = cm.new_source_file(
-            Arc::new(FileName::Custom("test.ts".into())),
+            Lrc::new(FileName::Custom("test.ts".into())),
             r#"
                 const foo = 1;
                 // @ALLOW-UNUSED-EXPORT
@@ -52,10 +51,10 @@ mod test {
 
     #[test]
     fn test_allowed_unused_export_named_as_bar() {
-        let cm = Arc::<SourceMap>::default();
+        let cm = Lrc::<SourceMap>::default();
         let comments = SingleThreadedComments::default();
         let fm = cm.new_source_file(
-            Arc::new(FileName::Custom("test.ts".into())),
+            Lrc::new(FileName::Custom("test.ts".into())),
             r#"
                 const foo = 1;
                 // @ALLOW-UNUSED-EXPORT
@@ -77,10 +76,10 @@ mod test {
     }
     #[test]
     fn test_allowed_unused_export_default() {
-        let cm = Arc::<SourceMap>::default();
+        let cm = Lrc::<SourceMap>::default();
         let comments = SingleThreadedComments::default();
         let fm = cm.new_source_file(
-            Arc::new(FileName::Custom("test.ts".into())),
+            Lrc::new(FileName::Custom("test.ts".into())),
             r#"
                 const foo = 1;
                 // @ALLOW-UNUSED-EXPORT
@@ -107,10 +106,10 @@ mod test {
 
     #[test]
     fn test_allowed_unused_export_kind_as_default() {
-        let cm = Arc::<SourceMap>::default();
+        let cm = Lrc::<SourceMap>::default();
         let comments = SingleThreadedComments::default();
         let fm = cm.new_source_file(
-            Arc::new(FileName::Custom("test.ts".into())),
+            Lrc::new(FileName::Custom("test.ts".into())),
             r#"
                 interface Foo {
                     bar: boolean;
@@ -134,10 +133,10 @@ mod test {
 
     #[test]
     fn test_allowed_unused_export_default_execution() {
-        let cm = Arc::<SourceMap>::default();
+        let cm = Lrc::<SourceMap>::default();
         let comments = SingleThreadedComments::default();
         let fm = cm.new_source_file(
-            Arc::new(FileName::Custom("test.ts".into())),
+            Lrc::new(FileName::Custom("test.ts".into())),
             r#"
                 function foo() { return 1; }
                 // @ALLOW-UNUSED-EXPORT
@@ -164,10 +163,10 @@ mod test {
 
     #[test]
     fn test_allowed_unused_export_default_class() {
-        let cm = Arc::<SourceMap>::default();
+        let cm = Lrc::<SourceMap>::default();
         let comments = SingleThreadedComments::default();
         let fm = cm.new_source_file(
-            Arc::new(FileName::Custom("test.ts".into())),
+            Lrc::new(FileName::Custom("test.ts".into())),
             r#"
                 // @ALLOW-UNUSED-EXPORT
                 export default class Foo {}
@@ -193,10 +192,10 @@ mod test {
 
     #[test]
     fn test_allowed_unused_export_const() {
-        let cm = Arc::<SourceMap>::default();
+        let cm = Lrc::<SourceMap>::default();
         let comments = SingleThreadedComments::default();
         let fm = cm.new_source_file(
-            Arc::new(FileName::Custom("test.ts".into())),
+            Lrc::new(FileName::Custom("test.ts".into())),
             r#"
                 // @ALLOW-UNUSED-EXPORT
                 export const foo = 1;
@@ -215,10 +214,10 @@ mod test {
 
     #[test]
     fn test_allowed_unused_export_from() {
-        let cm = Arc::<SourceMap>::default();
+        let cm = Lrc::<SourceMap>::default();
         let comments = SingleThreadedComments::default();
         let fm = cm.new_source_file(
-            Arc::new(FileName::Custom("test.ts".into())),
+            Lrc::new(FileName::Custom("test.ts".into())),
             r#"
                 // @ALLOW-UNUSED-EXPORT
                 export { foo } from './foo';
@@ -237,10 +236,10 @@ mod test {
 
     #[test]
     fn test_allowed_unused_export_default_from() {
-        let cm = Arc::<SourceMap>::default();
+        let cm = Lrc::<SourceMap>::default();
         let comments = SingleThreadedComments::default();
         let fm = cm.new_source_file(
-            Arc::new(FileName::Custom("test.ts".into())),
+            Lrc::new(FileName::Custom("test.ts".into())),
             r#"
                 // @ALLOW-UNUSED-EXPORT
                 export { default as foo } from './foo';
@@ -258,10 +257,10 @@ mod test {
 
     #[test]
     fn test_allowed_unused_export_star_from() {
-        let cm = Arc::<SourceMap>::default();
+        let cm = Lrc::<SourceMap>::default();
         let comments = SingleThreadedComments::default();
         let fm = cm.new_source_file(
-            Arc::new(FileName::Custom("test.ts".into())),
+            Lrc::new(FileName::Custom("test.ts".into())),
             r#"
                 // @ALLOW-UNUSED-EXPORT
                 export * from './foo';
@@ -280,9 +279,9 @@ mod test {
 
     #[test]
     fn test_export_named() {
-        let cm = Arc::<SourceMap>::default();
+        let cm = Lrc::<SourceMap>::default();
         let fm = cm.new_source_file(
-            Arc::new(FileName::Custom("test.ts".into())),
+            Lrc::new(FileName::Custom("test.ts".into())),
             r#"
             const foo = 1;
             export { foo }
@@ -309,9 +308,9 @@ mod test {
 
     #[test]
     fn test_allow_unused_export_and_collect_not_marked_export() {
-        let cm = Arc::<SourceMap>::default();
+        let cm = Lrc::<SourceMap>::default();
         let fm = cm.new_source_file(
-            Arc::new(FileName::Custom("test.ts".into())),
+            Lrc::new(FileName::Custom("test.ts".into())),
             r#"
             // some comment
             const foo = 1;
@@ -348,9 +347,9 @@ mod test {
 
     #[test]
     fn test_allow_unused_export_and_collect_not_marked_export_default() {
-        let cm = Arc::<SourceMap>::default();
+        let cm = Lrc::<SourceMap>::default();
         let fm = cm.new_source_file(
-            Arc::new(FileName::Custom("test.ts".into())),
+            Lrc::new(FileName::Custom("test.ts".into())),
             r#"
             // some comment
             const foo = 1;
@@ -388,9 +387,9 @@ mod test {
 
     #[test]
     fn test_allow_unused_export_default_and_collect_not_marked_named_export() {
-        let cm = Arc::<SourceMap>::default();
+        let cm = Lrc::<SourceMap>::default();
         let fm = cm.new_source_file(
-            Arc::new(FileName::Custom("test.ts".into())),
+            Lrc::new(FileName::Custom("test.ts".into())),
             r#"
             // some comment
             const foo = 1;
@@ -425,9 +424,9 @@ mod test {
 
     #[test]
     fn test_export_named_as_bar() {
-        let cm = Arc::<SourceMap>::default();
+        let cm = Lrc::<SourceMap>::default();
         let fm = cm.new_source_file(
-            Arc::new(FileName::Custom("test.ts".into())),
+            Lrc::new(FileName::Custom("test.ts".into())),
             r#"
             const foo = 1;
             export { foo as bar }
@@ -454,9 +453,9 @@ mod test {
 
     #[test]
     fn test_export_default() {
-        let cm = Arc::<SourceMap>::default();
+        let cm = Lrc::<SourceMap>::default();
         let fm = cm.new_source_file(
-            Arc::new(FileName::Custom("test.ts".into())),
+            Lrc::new(FileName::Custom("test.ts".into())),
             r#"
             const foo = 1;
             export default foo;
@@ -483,9 +482,9 @@ mod test {
 
     #[test]
     fn test_export_kind_as_default() {
-        let cm = Arc::<SourceMap>::default();
+        let cm = Lrc::<SourceMap>::default();
         let fm = cm.new_source_file(
-            Arc::new(FileName::Custom("test.ts".into())),
+            Lrc::new(FileName::Custom("test.ts".into())),
             r#"
             interface Foo {
                 bar: boolean;
@@ -514,9 +513,9 @@ mod test {
 
     #[test]
     fn test_export_default_execution() {
-        let cm = Arc::<SourceMap>::default();
+        let cm = Lrc::<SourceMap>::default();
         let fm = cm.new_source_file(
-            Arc::new(FileName::Custom("test.ts".into())),
+            Lrc::new(FileName::Custom("test.ts".into())),
             r#"
             function foo() { return 1; }
             export default foo();
@@ -543,9 +542,9 @@ mod test {
 
     #[test]
     fn test_export_default_class() {
-        let cm = Arc::<SourceMap>::default();
+        let cm = Lrc::<SourceMap>::default();
         let fm = cm.new_source_file(
-            Arc::new(FileName::Custom("test.ts".into())),
+            Lrc::new(FileName::Custom("test.ts".into())),
             r#"
             export default class Foo {}
             "#
@@ -571,9 +570,9 @@ mod test {
 
     #[test]
     fn test_export_const() {
-        let cm = Arc::<SourceMap>::default();
+        let cm = Lrc::<SourceMap>::default();
         let fm = cm.new_source_file(
-            Arc::new(FileName::Custom("test.ts".into())),
+            Lrc::new(FileName::Custom("test.ts".into())),
             r#"
             export const foo = 1;
             "#
@@ -599,9 +598,9 @@ mod test {
 
     #[test]
     fn test_export_from() {
-        let cm = Arc::<SourceMap>::default();
+        let cm = Lrc::<SourceMap>::default();
         let fm = cm.new_source_file(
-            Arc::new(FileName::Custom("test.ts".into())),
+            Lrc::new(FileName::Custom("test.ts".into())),
             r#"
             export { foo } from './foo';
             "#
@@ -626,9 +625,9 @@ mod test {
 
     #[test]
     fn test_export_default_from() {
-        let cm = Arc::<SourceMap>::default();
+        let cm = Lrc::<SourceMap>::default();
         let fm = cm.new_source_file(
-            Arc::new(FileName::Custom("test.ts".into())),
+            Lrc::new(FileName::Custom("test.ts".into())),
             r#"
             export { default as foo } from './foo';
             "#
@@ -650,9 +649,9 @@ mod test {
 
     #[test]
     fn test_export_star_from() {
-        let cm = Arc::<SourceMap>::default();
+        let cm = Lrc::<SourceMap>::default();
         let fm = cm.new_source_file(
-            Arc::new(FileName::Custom("test.ts".into())),
+            Lrc::new(FileName::Custom("test.ts".into())),
             r#"
             export * from './foo';
             "#
@@ -673,9 +672,9 @@ mod test {
 
     #[test]
     fn test_import_default() {
-        let cm = Arc::<SourceMap>::default();
+        let cm = Lrc::<SourceMap>::default();
         let fm = cm.new_source_file(
-            Arc::new(FileName::Custom("test.ts".into())),
+            Lrc::new(FileName::Custom("test.ts".into())),
             r#"
             import foo from './foo';
             "#
@@ -694,9 +693,9 @@ mod test {
 
     #[test]
     fn test_import_specifier() {
-        let cm = Arc::<SourceMap>::default();
+        let cm = Lrc::<SourceMap>::default();
         let fm = cm.new_source_file(
-            Arc::new(FileName::Custom("test.ts".into())),
+            Lrc::new(FileName::Custom("test.ts".into())),
             r#"
             import {foo} from './foo';
             "#
@@ -716,9 +715,9 @@ mod test {
 
     #[test]
     fn test_import_specifier_with_alias() {
-        let cm = Arc::<SourceMap>::default();
+        let cm = Lrc::<SourceMap>::default();
         let fm = cm.new_source_file(
-            Arc::new(FileName::Custom("test.ts".into())),
+            Lrc::new(FileName::Custom("test.ts".into())),
             r#"
             import {foo as bar} from './foo';
             "#
@@ -738,9 +737,9 @@ mod test {
 
     #[test]
     fn test_import_default_with_alias() {
-        let cm = Arc::<SourceMap>::default();
+        let cm = Lrc::<SourceMap>::default();
         let fm = cm.new_source_file(
-            Arc::new(FileName::Custom("test.ts".into())),
+            Lrc::new(FileName::Custom("test.ts".into())),
             r#"
             import {default as foo} from './foo';
             "#
@@ -759,9 +758,9 @@ mod test {
 
     #[test]
     fn test_import_call() {
-        let cm = Arc::<SourceMap>::default();
+        let cm = Lrc::<SourceMap>::default();
         let fm = cm.new_source_file(
-            Arc::new(FileName::Custom("test.ts".into())),
+            Lrc::new(FileName::Custom("test.ts".into())),
             r#"
             const lazyModule = new LazyModule(() => import(/* webpackChunkName: "mailStore" */ './foo'));
             export const lazyModule = new LazyModule(
@@ -785,9 +784,9 @@ mod test {
 
     #[test]
     fn test_import_default_and_specifier() {
-        let cm = Arc::<SourceMap>::default();
+        let cm = Lrc::<SourceMap>::default();
         let fm = cm.new_source_file(
-            Arc::new(FileName::Custom("test.ts".into())),
+            Lrc::new(FileName::Custom("test.ts".into())),
             r#"
             import foo, {bar} from './foo';
             "#
@@ -807,9 +806,9 @@ mod test {
 
     #[test]
     fn test_import_star() {
-        let cm = Arc::<SourceMap>::default();
+        let cm = Lrc::<SourceMap>::default();
         let fm = cm.new_source_file(
-            Arc::new(FileName::Custom("test.ts".into())),
+            Lrc::new(FileName::Custom("test.ts".into())),
             r#"
             import * as foo from './foo';
             "#
@@ -828,9 +827,9 @@ mod test {
 
     #[test]
     fn test_require() {
-        let cm = Arc::<SourceMap>::default();
+        let cm = Lrc::<SourceMap>::default();
         let fm = cm.new_source_file(
-            Arc::new(FileName::Custom("test.ts".into())),
+            Lrc::new(FileName::Custom("test.ts".into())),
             r#"
             const foo = require('./foo');
             "#
@@ -848,9 +847,9 @@ mod test {
 
     #[test]
     fn test_import_equals() {
-        let cm = Arc::<SourceMap>::default();
+        let cm = Lrc::<SourceMap>::default();
         let fm = cm.new_source_file(
-            Arc::new(FileName::Custom("test.ts".into())),
+            Lrc::new(FileName::Custom("test.ts".into())),
             r#"
             import foo = require('./foo')
             "#
@@ -868,9 +867,9 @@ mod test {
 
     #[test]
     fn test_import_statement() {
-        let cm = Arc::<SourceMap>::default();
+        let cm = Lrc::<SourceMap>::default();
         let fm = cm.new_source_file(
-            Arc::new(FileName::Custom("test.ts".into())),
+            Lrc::new(FileName::Custom("test.ts".into())),
             r#"
             import './foo'
             "#
