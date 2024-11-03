39d5eb35581ffb97b77b8cd82bfa20a56b2d2625
Maxwell Huang-Hobbs <mhuan13@gmail.com>
2024-11-03 19:17:01 +0000

    set cargo term verbose in builds

--- crates/napi_root/src/lib.rs
+++ crates/napi_root/src/lib.rs
@@ -1,3 +1,5 @@
+// These are disabled in 'test' because napi indirectly references
+// symbols from node, which would cause the test binaries to fail to link.
 #[cfg(not(test))]
 mod _module {
     pub use good_fences_napi::*;
